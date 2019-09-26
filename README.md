4047778bfdc08b08f309971891f8e9939d3475db
Paulo de Pinho <prdepinho@gmail.com>
2019-09-25 21:45:02 -0300

    finish

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # calendar
-A simple calendar app
+A simple calendar app.
+
+The current day is colored blue.
+
+To add a schedule, edit the dicionary at schedule.py and the
+program will show it in red in the calendar, and the description
+will be in the bottom.
+
+To see the current month:
+
+```
+py cal.py
+```
+
+To see a certain month (october in this case):
+
+```
+py cal.py -m 10
+```
+
+To see a certain month in a certain year:
+
+```
+py cal.py -m 10 -y 2020
+```
+
+To see a couple of months in advance in additioin to the chosen month:
+
+```
+py cal.py -m 10 -y 2020 -a 2
+```
--- cal.py
+++ cal.py
@@ -1,6 +1,8 @@
 import calendar
 import datetime
 import colorama
+import sys
+import getopt
 from schedule import schedule
 
 cal = calendar.Calendar(6)
@@ -8,57 +10,102 @@ today = datetime.date.today()
 
 year = today.year
 month = today.month
-monthdays = [x for x in cal.itermonthdays2(year, month)]
+advance = 0
 
-calstr = ""
-calstr += today.strftime("%B, %Y")
-calstr += "\n"
-calstr += "%+6s" % "sun"
-calstr += "%+6s" % "mon"
-calstr += "%+6s" % "tue"
-calstr += "%+6s" % "wed"
-calstr += "%+6s" % "thu"
-calstr += "%+6s" % "fri"
-calstr += "%+6s" % "sat"
-calstr += "\n"
+try:
+    optlist, args = getopt.getopt(
+            sys.argv[1:],
+            'm:y:a:',
+            ['month=', 'year=', 'advance=', 'help'])
 
-datesstr = ""
+except getopt.GetoptError as e:
+    print(e)
+    exit()
 
-week = 1
-iday = 0
-while iday < len(monthdays):
-    for _ in range(7):
-        day = monthdays[iday][0]
-        # import pdb; pdb.set_trace()
-        if day == 0:
-            calstr += "%+6s" % ""
+for o, a in optlist:
+    if o in ['-m', '--month']:
+        month = int(a)
+        if month not in range(1, 13):
+            print("Month must be between 1 and 12.")
+            exit()
+    elif o in ['-y', '--year']:
+        year = int(a)
+    elif o in ['-a', '--advance']:
+        advance = int(a)
+    elif o == '--help':
+        print('%-40s %s' % ('-m, --month [month]:', 'Show the selected month.'))
+        print('%-40s %s' % ('-y, --year [year]:', 'Show the selected year.'))
+        print('%-40s %s' % ('-a, --advance [months]:', 'Show months in advance.'))
+        exit()
+    else:
+        print('unhandled option: %s' % o)
 
-        else:
-            scheduled = datetime.date(year, month, day) in [datetime.date(year, month, d) for d in schedule[year][month]]
 
-            if today == datetime.date(year, month, day):
-                calstr += colorama.Fore.GREEN + "%+4s" % ">"
-                calstr += colorama.Style.RESET_ALL
 
-                if scheduled:
-                    calstr += colorama.Fore.RED + "%s" % str(day)
-                    calstr += colorama.Style.RESET_ALL
-                    datesstr += "%6d: %s\n" % (day, schedule[year][month][day])
-                else:
-                    calstr += colorama.Fore.GREEN + "%s" % str(day)
-                    calstr += colorama.Style.RESET_ALL
+def print_month(month, year):
+    monthdays = [x for x in cal.itermonthdays2(year, month)]
 
-            elif scheduled:
-                calstr += colorama.Fore.RED + "%+6s" % str(day)
-                calstr += colorama.Style.RESET_ALL
-                datesstr += "%6d: %s\n" % (day, schedule[year][month][day])
+    calstr = ""
+    calstr += datetime.date(year, month, 1).strftime("%B, %Y")
+    calstr += "\n"
+    calstr += "%+6s" % "sun"
+    calstr += "%+6s" % "mon"
+    calstr += "%+6s" % "tue"
+    calstr += "%+6s" % "wed"
+    calstr += "%+6s" % "thu"
+    calstr += "%+6s" % "fri"
+    calstr += "%+6s" % "sat"
+    calstr += "\n"
+
+    datesstr = ""
+
+    week = 1
+    iday = 0
+    while iday < len(monthdays):
+        for _ in range(7):
+            day = monthdays[iday][0]
+            if day == 0:
+                calstr += "%+6s" % ""
 
             else:
-                calstr += "%+6s" % str(day)
+                if year in schedule and month in schedule[year]:
+                    scheduled = datetime.date(year, month, day) in [datetime.date(year, month, d) for d in schedule[year][month]]
+                else:
+                    scheduled = False
 
-        iday += 1
-    calstr += "\n"
-    week += 1
+                if today == datetime.date(year, month, day):
+                    calstr += colorama.Fore.GREEN + "%+4s" % ">"
+                    calstr += colorama.Style.RESET_ALL
 
-print(calstr)
-print(datesstr)
+                    if scheduled:
+                        calstr += colorama.Fore.RED + colorama.Style.BRIGHT + "%s" % str(day)
+                        calstr += colorama.Style.RESET_ALL
+                        datesstr += "%6d: %s\n" % (day, schedule[year][month][day])
+                    else:
+                        calstr += colorama.Fore.GREEN + "%s" % str(day)
+                        calstr += colorama.Style.RESET_ALL
+
+                elif scheduled:
+                    calstr += colorama.Fore.RED + colorama.Style.BRIGHT + "%+6s" % str(day)
+                    calstr += colorama.Style.RESET_ALL
+                    datesstr += "%6d: %s\n" % (day, schedule[year][month][day])
+
+                else:
+                    calstr += "%+6s" % str(day)
+
+            iday += 1
+        calstr += "\n"
+        week += 1
+
+    print(calstr)
+    print(datesstr)
+
+
+if __name__ == "__main__":
+    print_month(month, year)
+    if advance > 0:
+        for a in range(1, advance + 1):
+            if month + a > 12:
+                year += 1
+                month = 1
+            print_month(month + a, year)
